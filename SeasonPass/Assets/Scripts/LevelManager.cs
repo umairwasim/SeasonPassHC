7a1342abb96bc04ac84eaff4026656fd82d221ef
Umair <umairwasim001@gmail.com>
2023-11-20 22:10:10 +0500

    Update LevelManager.cs

--- SeasonPass/Assets/Scripts/LevelManager.cs
+++ SeasonPass/Assets/Scripts/LevelManager.cs
@@ -1,33 +1,33 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-namespace BhorGames
+//namespace BhorGames
+//{
+public class LevelManager : MonoBehaviour
 {
-    public class LevelManager : MonoBehaviour
+    public static LevelManager Instance;
+    public int currentLevel;
+
+    void Awake()
     {
-        public static LevelManager Instance;
-        public int currentLevel;
+        if (Instance == null) Instance = this;
+    }
 
-        void Awake()
-        {
-            if (Instance == null) Instance = this;
-        }
+    void Start()
+    {
+        UIManager.Instance.UpdateLevelTxt();
+    }
 
-        void Start()
-        {
-            UIManager.Instance.UpdateLevelTxt();
-        }
+    public void NextLevel()
+    {
+        currentLevel += 1;
+        PlayerPrefs.SetInt("level", currentLevel);
+        SceneManager.LoadScene((currentLevel % (SceneManager.sceneCountInBuildSettings - 1)) + 1);
+    }
 
-        public void NextLevel()
-        {
-            currentLevel += 1;
-            PlayerPrefs.SetInt("level", currentLevel);
-            SceneManager.LoadScene((currentLevel % (SceneManager.sceneCountInBuildSettings - 1)) + 1);
-        }
-
-        public void RestartLevel()
-        {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-        }
+    public void RestartLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
+//}
