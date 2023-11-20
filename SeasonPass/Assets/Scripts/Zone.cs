175c15fab8a18a6a807aca0f7a23438d1ee9dd85
Umair <umairwasim001@gmail.com>
2023-11-20 22:10:41 +0500

    Subtle changes

--- SeasonPass/Assets/Scripts/Zone.cs
+++ SeasonPass/Assets/Scripts/Zone.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class Zone : MonoBehaviour
 {
     private const string PLAYER = "Player";
+   
     public Clothes linkedClothes;
 
     private void OnTriggerEnter(Collider other)
